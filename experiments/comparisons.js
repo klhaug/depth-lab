46e77234f21690b3489c65aab346a69d28844e5f
klhaug <kristian@snougle.no>
2026-02-24 11:03:44 +0100

    Still figuring out structure

--- experiments/comparisons.js
+++ experiments/comparisons.js
@@ -2,8 +2,25 @@ const age = 12
 
 console.log("TESTING COMPARISONS")
 console.log("--------------------")
+console.log("Values")
 
 
-const boolean = age === 12
+let myName = "Kristian"
 
-console.log(boolean)
+let myFirstName = myName
+
+myName = 'Anniken'
+
+console.log("myName:", myName)
+console.log("myFirstName:", myFirstName)
+
+console.log("--------------------")
+console.log("References")
+
+let firstArray = [1, 0, 2, 3]
+let secondArray = firstArray
+
+firstArray[0] = 69
+
+console.log("firstArray", firstArray)
+console.log("secondArray", secondArray)
